054d7cdcc835b4967b35005b3fcf4437ac6de032
Boban <mirkovic.sloba@gmail.com>
2016-03-13 20:23:45 +0100

    new api id

--- app/weather.services.ts
+++ app/weather.services.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs/Rx';
 export class WeatherService{
      constructor(private _http: Http) { } 
     getWeather() {
-         return this._http.get('http://api.openweathermap.org/data/2.5/find?lat=44.39&lon=20.26&cnt=10&appid=44db6a862fba0b067b1930da0d769e98')
+         return this._http.get('http://api.openweathermap.org/data/2.5/find?lat=44.39&lon=20.26&cnt=10&appid=b1b15e88fa797225412429c1c50c122a')
       .map((response: Response) => response.json().list)
       .catch(this.handleError);
     }
