b2af10975db85d8ca80d029b0e19f47193728a01
Slobodan Mirkovic <mirkovic.sloba@gmail.com>
2016-05-09 21:50:50 +0200

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,32 +1,43 @@
+/**
+ * PLUNKER VERSION (based on systemjs.config.js in angular.io)
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ * Override at the last minute with global.filterSystemConfig (as plunkers do)
+ */
 (function(global) {
 
-  // map tells the System loader where to look for things
-  var map = {
+  var ngVer = '@2.0.0-rc.1'; // lock in the angular package version; do not let it float to current!
+
+  //map tells the System loader where to look for things
+  var  map = {
     'app':                        'app', // 'dist',
-    'rxjs':                       'node_modules/rxjs',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    '@angular':                   'node_modules/@angular'
+    'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
+    'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api' // get latest
   };
 
-  // packages tells the System loader how to load when no filename and/or no extension
+  //packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'app':                        { main: 'main.ts',  defaultExtension: 'ts' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { defaultExtension: 'js' },
   };
 
   var packageNames = [
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/core',
-    '@angular/http',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/router',
-    '@angular/router-deprecated',
-    '@angular/testing',
-    '@angular/upgrade',
+      '@angular/common',
+      '@angular/compiler',
+      '@angular/core',
+      '@angular/http',
+      '@angular/platform-browser',
+      '@angular/platform-browser-dynamic',
+      '@angular/router',
+      '@angular/router-deprecated',
+      '@angular/upgrade',
   ];
+
+  // add map entries for angular packages in the form '@angular/common': 'https://npmcdn.com/@angular/common@0.0.0-3?main=browser'
+  packageNames.forEach(function(pkgName) {
+    map[pkgName] = 'https://npmcdn.com/' + pkgName + ngVer;
+  });
 
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
   packageNames.forEach(function(pkgName) {
@@ -34,6 +45,10 @@
   });
 
   var config = {
+    transpiler: 'typescript',
+    typescriptOptions: {
+      emitDecoratorMetadata: true
+    },
     map: map,
     packages: packages
   }
