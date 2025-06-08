ce332978a46d02890116515df44110664a359c2d
Slobodan Mirkovic <mirkovic.sloba@gmail.com>
2025-06-08 12:33:29 +0200

    change to https

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -13,7 +13,7 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-    private weatherUrl = 'http://api.openweathermap.org/data/2.5/find?lat=44.39&lon=20.26&cnt=10&appid=8e200a8cf713820d46d186869d7a4dcb';
+    private weatherUrl = 'https://api.openweathermap.org/data/2.5/find?lat=44.39&lon=20.26&cnt=10&appid=8e200a8cf713820d46d186869d7a4dcb';
 
     getWeather () {
       return this.http.get<any>(this.weatherUrl);
